5074ad381659d7d49c2ed173e28a336f8c7d00a6
David COLEMAN <colemandavid2000@gmail.com>
2024-04-26 16:57:32 +0200

    commit

--- gestion_etudiants/views.py
+++ gestion_etudiants/views.py
@@ -14,20 +14,25 @@ class StudentViewSet(viewsets.ModelViewSet):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def retrieve(self, request, pk=None):
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None, *args, **kwargs):
         queryset = self.get_queryset()
         student = queryset.get(pk=pk)
         serializer = self.get_serializer(student)
         return Response(serializer.data)
 
-    def update(self, request, pk=None):
+    def update(self, request, pk=None, *args, **kwargs):
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
 
-    def destroy(self, request, pk=None):
+    def destroy(self, request, pk=None, *args, **kwargs):
         instance = self.get_object()
         instance.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
